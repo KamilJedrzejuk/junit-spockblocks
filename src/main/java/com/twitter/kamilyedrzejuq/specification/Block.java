8f33fa377155c2580f4d0225bedeedb54c74f20f
Kamil Jedrzejuk <kamil.jedrzejuk@gmail.com>
2019-08-14 11:08:29 +0200

    Added a ASM library and Block enum which represents a possible byte code instruction of each block method. The block methods are static void.

---
+++ src/main/java/com/twitter/kamilyedrzejuq/specification/Block.java
@@ -0,0 +1,31 @@
+package com.twitter.kamilyedrzejuq.specification;
+
+/**
+ * Reprensets a possible blocks in test methods.
+ * Each block have a corresponding byte code instruction for a static method from {@link Specification} class.
+ * NOTE!!!
+ * Actually is a prefix of instruction, the instruction has also information about
+ * argument and return type like below:
+ * <pre>
+ * {@code
+ *   INVOKESTATIC com/twitter/kamilyedrzejuq/BlockTest.GIVEN (Ljava/lang/String;)V
+ * }
+ * or without argument:
+ * {@code
+ *   INVOKESTATIC com/twitter/kamilyedrzejuq/BlockTest.GIVEN ()V
+ * }
+ * </pre>
+ */
+public enum  Block {
+
+    GIVEN("INVOKESTATIC com/twitter/kamilyedrzejuq/BlockTest.GIVEN"),
+    WHEN("INVOKESTATIC com/twitter/kamilyedrzejuq/BlockTest.WHEN"),
+    THEN("INVOKESTATIC com/twitter/kamilyedrzejuq/BlockTest.THEN"),
+    EXPECT("INVOKESTATIC com/twitter/kamilyedrzejuq/BlockTest.EXPECT"),
+    AND("INVOKESTATIC com/twitter/kamilyedrzejuq/BlockTest.AND");
+
+    private final String byteCodeInstruction;
+    Block(String instruction) {
+        byteCodeInstruction = instruction;
+    }
+}
